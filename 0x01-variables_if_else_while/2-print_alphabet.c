af9e11e08bdf14855f6193391545353d44e26ff7
Yibeltal Ebabu <yibeltal2127@gmail.com>
2022-06-08 08:29:30 -0700

    2-alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
-
-
-
+#include <stdlib.h>
+#include <time.h>
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	char low;
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		printf("\n");
 
-			return (0);
-
+	return (0);
 }
