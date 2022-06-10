90c9ba32d737197ea391918808c00eecd3615eab
Yibeltal Ebabu <yibeltal2127@gmail.com>
2022-06-10 03:19:45 -0700

    f

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,12 +4,8 @@
  */
 void print_numbers(void)
 {
-	int i = 0;
-
-	while (i <= 9)
-	{
-		_putchar(i + '0');
-		++i;
-	}
-	_putchar('\n');
+int i;
+for (i = 0; i <= 9; i++)
+_putchar(i + '0');
+_putchar('\n');
 }
